a9e28c719632a72e2107f82ae3c3efaaf4e2cb98
yusufcanbircan <yusufcannnz@hotmail.com>
2022-09-07 11:26:54 +0300

    first commit

---
+++ viewController/viewController/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  viewController
+//
+//  Created by Yusuf Can Bircan on 7.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func button(_ sender: Any) {
+    }
+    
+
+}
+
