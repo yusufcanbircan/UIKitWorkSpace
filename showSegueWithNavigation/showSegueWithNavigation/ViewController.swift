c0c167169b765a311810f3aac832d7a39b69e128
yusufcanbircan <yusufcannnz@hotmail.com>
2022-09-08 14:57:34 +0300

    ff

---
+++ showSegueWithNavigation/showSegueWithNavigation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  showSegueWithNavigation
+//
+//  Created by Yusuf Can Bircan on 8.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
