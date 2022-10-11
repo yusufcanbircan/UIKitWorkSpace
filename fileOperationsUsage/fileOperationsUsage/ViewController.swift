16c33eec72d7225b720d86ede5cbe3a8b33e2155
yusufcanbircan <yusufcannnz@hotmail.com>
2022-10-11 14:56:25 +0300

    write read delete usage

--- fileOperationsUsage/fileOperationsUsage/ViewController.swift
+++ fileOperationsUsage/fileOperationsUsage/ViewController.swift
@@ -8,12 +8,75 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func writeButton(_ sender: Any) {
+        
+        let message: String = textField.text!
+        
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+            let filePath = dir.appendingPathExtension("file.txt")
+            
+            do {
+                
+                try message.write(to: filePath, atomically: false, encoding: String.Encoding.utf8)
+                
+                textField.text = ""
+                
+            } catch {
+                print("Error while writing to path!")
+            }
+        }
+    }
+    
+    @IBAction func readButton(_ sender: Any) {
+        
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+            let filePath = dir.appendingPathExtension("file.txt")
+            
+            do {
+                
+                textField.text = try String(contentsOf: filePath, encoding: String.Encoding.utf8)
+                
+            } catch {
+                
+                print("Error while reading!")
+            
+            }
+        }
+        
+    }
+    
+    @IBAction func deleteButton(_ sender: Any) {
+        
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+            let filePath = dir.appendingPathExtension("file.txt")
+            if FileManager.default.fileExists(atPath: filePath.path) {
+                do {
+                    try FileManager.default.removeItem(at: filePath)
+                    textField.text = ""
+                } catch {
+                    
+                }
+            }
+        }
+        
+    }
+    
+    @IBAction func writeImageButton(_ sender: Any) {
+    }
+    
+    @IBAction func showImageButton(_ sender: Any) {
+    }
+    
+    @IBAction func deleteImageButton(_ sender: Any) {
+    }
 }
 
