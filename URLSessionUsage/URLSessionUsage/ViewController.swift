b2cf8099b788382058c4fe85f52b35e67dc5d473
yusufcanbircan <yusufcannnz@hotmail.com>
2023-01-21 18:27:01 +0300

    fetch all and search with text

--- URLSessionUsage/URLSessionUsage/ViewController.swift
+++ URLSessionUsage/URLSessionUsage/ViewController.swift
@@ -14,7 +14,9 @@ class ViewController: UIViewController {
         
         //addPerson()
         //updatePerson(id: 14226, ad: "newYusuf", tel: 404040)
-        deletePerson(id: 14226)
+        //deletePerson(id: 14226)
+        //fetchAll()
+        search(searchText: "a")
     }
     
     func addPerson() {
@@ -75,6 +77,56 @@ class ViewController: UIViewController {
         request.httpMethod = "POST"
         
         let postString = "kisi_id=\(id)"
+        request.httpBody = postString.data(using: .utf8)
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            if error != nil || data == nil {
+                print("error")
+                return
+            }
+            
+            do {
+                
+                if let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [String: Any] {
+                    print(json)
+                }
+                
+            } catch {
+                print(error.localizedDescription)
+            }
+            
+        }.resume()
+    }
+    
+    func fetchAll() {
+        let url = URL(string: "http://kasimadalan.pe.hu/kisiler/tum_kisiler.php")!
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            if error != nil || data == nil {
+                print("error")
+                return
+            }
+            
+            do {
+                
+                if let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [String: Any] {
+                    print(json)
+                }
+                
+            } catch {
+                print(error.localizedDescription)
+            }
+            
+        }.resume()
+    }
+    
+    func search(searchText: String) {
+        var request = URLRequest(url: URL(string: "http://kasimadalan.pe.hu/kisiler/tum_kisiler_arama.php")!)
+        request.httpMethod = "POST"
+        
+        let postString = "kisi_ad=\(searchText)"
         request.httpBody = postString.data(using: .utf8)
         
         URLSession.shared.dataTask(with: request) { (data, response, error) in
